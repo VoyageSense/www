69a2dfe4b5073949bfbf7dcd710afca230d18881
Alex <alex@sailvisionpro.com>
2025-05-01 15:37:51 -0700

    Revoke all store codes and issue twenty more
    We've moving to the new version of the site in prod.

--- src/net/sailvision/www/store/target.clj
+++ src/net/sailvision/www/store/target.clj
@@ -14,124 +14,144 @@
                            {:tahiti          "Tahiti"}}
                :price     300}]
     {:watermellon dummy
-     :mtklk dummy
-     :ulrbt dummy
-     :pwhom dummy
-     :ocbto dummy
-     :aocbq dummy
-     :bsfqn dummy
-     :ysquo dummy
-     :lhztx dummy
-     :xqccm dummy
-     :plpgm dummy
-     :mango {}
-     :1ayjd {}
+     ;; :mtklk dummy
+     ;; :ulrbt dummy
+     ;; :pwhom dummy
+     ;; :ocbto dummy
+     ;; :aocbq dummy
+     ;; :bsfqn dummy
+     ;; :ysquo dummy
+     ;; :lhztx dummy
+     ;; :xqccm dummy
+     ;; :plpgm dummy
+     ;; :mango {}
+     ;; :1ayjd {}
      ;; :1vhji {}
-     :1nzxl {}
-     :1rxah {}
-     :1ojpw {}
-     :1uvac {}
-     :1xppc {}
-     :1hhey {}
-     :1ruxy {}
-     :1hxpd {}
-     :1djii {}
-     :1pdqs {}
-     :1jxop {}
-     :1ovwc {}
-     :1eblo {}
-     :1psbx {}
-     :1bgpt {}
-     :1xjjv {}
-     :1kksd {}
-     :1nbnj {}
-     :1mkrj {}
-     :1khbf {}
-     :1fnji {}
-     :1jamh {}
-     :1smob {}
-     :1pajn {}
-     :1dqhp {}
-     :1kcnz {}
-     :1hqqo {}
-     :1fovf {}
-     :1rvxb {}
-     :1lpzj {}
-     :1urcs {}
-     :1gupz {}
-     :1terr {}
-     :1ddor {}
-     :1bnps {}
-     :1rsrs {}
-     :1sobx {}
-     :1pflq {}
-     :1ajyk {}
-     :1zevq {}
-     :1eyey {}
-     :1hlis {}
-     :1afqs {}
-     :1emdb {}
-     :1bncy {}
-     :1yhvz {}
-     :1donk {}
-     :1wcnc {}
-     :1beyc {}
-     :1knjj {}
-     :1wlda {}
-     :1boie {}
-     :1wzir {}
-     :1utcg {}
-     :1zexj {}
-     :1kfny {}
-     :1yypb {}
-     :1jrto {}
-     :1hwza {}
-     :1yunx {}
-     :1gglp {}
-     :1jnog {}
-     :1dopu {}
-     :1smaj {}
-     :1seja {}
-     :1xzuy {}
-     :1rdyf {}
-     :1obwb {}
-     :1rpsd {}
-     :1uokj {}
-     :1brka {}
-     :1wmef {}
-     :1cddg {}
-     :1rzsg {}
-     :1zxsc {}
-     :1sphl {}
-     :1lvql {}
-     :1mcpd {}
-     :1amcw {}
-     :1dnvv {}
-     :1gmvw {}
-     :1tpfp {}
-     :1cfjg {}
-     :1fcml {}
-     :1envx {}
-     :1xlel {}
-     :1rjza {}
-     :1rbrw {}
-     :1ixeo {}
-     :1fqap {}
-     :1aixh {}
-     :1drkr {}
-     :1ofoh {}
-     :1uscz {}
-     :1slxl {}
-     :1dijp {}
-     :1bdvq {}
-     :1mvyh {}
-     :1jbch {}
-     :1wdti {}
-     :1qaag {}
-     :1ttxc {}
-     :1wpai {}
-     :1ohvg {}
-     :1wabw {}
-     :1jatp {}
-     :1cxmm {}
-     :1kbsu {}}))
+     ;; :1nzxl {}
+     ;; :1rxah {}
+     ;; :1ojpw {}
+     ;; :1uvac {}
+     ;; :1xppc {}
+     ;; :1hhey {}
+     ;; :1ruxy {}
+     ;; :1hxpd {}
+     ;; :1djii {}
+     ;; :1pdqs {}
+     ;; :1jxop {}
+     ;; :1ovwc {}
+     ;; :1eblo {}
+     ;; :1psbx {}
+     ;; :1bgpt {}
+     ;; :1xjjv {}
+     ;; :1kksd {}
+     ;; :1nbnj {}
+     ;; :1mkrj {}
+     ;; :1khbf {}
+     ;; :1fnji {}
+     ;; :1jamh {}
+     ;; :1smob {}
+     ;; :1pajn {}
+     ;; :1dqhp {}
+     ;; :1kcnz {}
+     ;; :1hqqo {}
+     ;; :1fovf {}
+     ;; :1rvxb {}
+     ;; :1lpzj {}
+     ;; :1urcs {}
+     ;; :1gupz {}
+     ;; :1terr {}
+     ;; :1ddor {}
+     ;; :1bnps {}
+     ;; :1rsrs {}
+     ;; :1sobx {}
+     ;; :1pflq {}
+     ;; :1ajyk {}
+     ;; :1zevq {}
+     ;; :1eyey {}
+     ;; :1hlis {}
+     ;; :1afqs {}
+     ;; :1emdb {}
+     ;; :1bncy {}
+     ;; :1yhvz {}
+     ;; :1donk {}
+     ;; :1wcnc {}
+     ;; :1beyc {}
+     ;; :1knjj {}
+     ;; :1wlda {}
+     ;; :1boie {}
+     ;; :1wzir {}
+     ;; :1utcg {}
+     ;; :1zexj {}
+     ;; :1kfny {}
+     ;; :1yypb {}
+     ;; :1jrto {}
+     ;; :1hwza {}
+     ;; :1yunx {}
+     ;; :1gglp {}
+     ;; :1jnog {}
+     ;; :1dopu {}
+     ;; :1smaj {}
+     ;; :1seja {}
+     ;; :1xzuy {}
+     ;; :1rdyf {}
+     ;; :1obwb {}
+     ;; :1rpsd {}
+     ;; :1uokj {}
+     ;; :1brka {}
+     ;; :1wmef {}
+     ;; :1cddg {}
+     ;; :1rzsg {}
+     ;; :1zxsc {}
+     ;; :1sphl {}
+     ;; :1lvql {}
+     ;; :1mcpd {}
+     ;; :1amcw {}
+     ;; :1dnvv {}
+     ;; :1gmvw {}
+     ;; :1tpfp {}
+     ;; :1cfjg {}
+     ;; :1fcml {}
+     ;; :1envx {}
+     ;; :1xlel {}
+     ;; :1rjza {}
+     ;; :1rbrw {}
+     ;; :1ixeo {}
+     ;; :1fqap {}
+     ;; :1aixh {}
+     ;; :1drkr {}
+     ;; :1ofoh {}
+     ;; :1uscz {}
+     ;; :1slxl {}
+     ;; :1dijp {}
+     ;; :1bdvq {}
+     ;; :1mvyh {}
+     ;; :1jbch {}
+     ;; :1wdti {}
+     ;; :1qaag {}
+     ;; :1ttxc {}
+     ;; :1wpai {}
+     ;; :1ohvg {}
+     ;; :1wabw {}
+     ;; :1jatp {}
+     ;; :1cxmm {}
+     ;; :1kbsu {}
+     :2dlhi {}
+     :2brip {}
+     :2zuta {}
+     :2mnnt {}
+     :2bjii {}
+     :2pugv {}
+     :2pjem {}
+     :2ftzv {}
+     :2rwdf {}
+     :2vuvo {}
+     :2xknh {}
+     :2vznh {}
+     :2afsc {}
+     :2xmcs {}
+     :2pxlv {}
+     :2tvsf {}
+     :2oxqc {}
+     :2zojo {}
+     :2qskj {}
+     :2evie {}}))
