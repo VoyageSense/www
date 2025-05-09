a90804bee40054351f80a5b0732029d68dfcf9fc
Alex <alex@sailvisionpro.com>
2025-05-09 14:14:39 -0700

    Fix up the purchase form and remove image

--- src/net/sailvision/www/store/configuration.clj
+++ src/net/sailvision/www/store/configuration.clj
@@ -13,12 +13,12 @@
 (defn almanac-request [code]
   {:body [[:details (style {:margin-top "3em"})
            [:summary "Don&rsquo;t see your destination or boat?"]
-           [:p "Let us know where you&rsquo;re going and what you&rsquo;ll be sailing so we can start working on the almanac. We&rsquo;ll follow up once it&rsquo;s ready."]
+           [:p "Let us know where you&rsquo;re going and what you&rsquo;ll be sailing so we can start teaching Popai. We&rsquo;ll follow up once it&rsquo;s ready."]
            [:form.sku-request (merge {:action (route/with-code route/request-almanac code)
                                       :method :post}
                                      (style {:display               :grid
                                              :grid-template-columns "auto 1fr"
-                                             :gap                   "0.3em"
+                                             :gap                   "0.6em"
                                              :width                 :fit-content
                                              :margin                "auto"}))
             [:input {:type  :hidden
@@ -34,7 +34,8 @@
             [:button (merge {:type :submit}
                             (style {:grid-column  "span 2"
                                     :justify-self :center
-                                    :margin-top   "1em"})) "Request Almanac"]]]]})
+                                    :margin-top   "1em"
+                                    :padding      "0.3em 1em"})) "Request Almanac"]]]]})
 
 (defn configuration [code]
   (let [config    (code target/configs)
@@ -165,48 +166,44 @@
              [:details.faq
               [:summary "Does Popai consider my experience level?"]
               [:p "Popai learns through its interactions which topics and in which situations you prefer verbose explanations or directions, versus a minimal hint or nothing at all."]]
-             [:div (style {:display    :flex
-                           :flex-wrap  :wrap
-                           :margin-top "5em"})
-              [:img (merge {:src "/person.svg"}
-                           (style {:flex         "1 1 50%"
-                                   :aspect-ratio 1}))]
-              [:div.soft-outline (style {:display        :flex
-                                         :flex-direction :column
-                                         :flex           "1 1 50%"
-                                         :height         :min-content})
-               [:h1 (style {:font-size "1.5em"
-                            :margin    "0em auto 1em"}) "Popai Digital Almanac"]
-               [:form.sku-selection (merge {:action (route/with-code route/checkout code)
-                                            :method :post}
-                                           (style {:display               :grid
-                                                   :grid-template-columns "auto 1fr"
-                                                   :gap                   "0.6em"
-                                                   :margin                "auto"}))
-                [:input {:type  :hidden
-                         :name  :product
-                         :value :popai}]
-                [:label           {:for  :location} "Location:"]
-                [:select#location (merge {:name :location}
-                                         (style {:min-width 0}))
-                 [:option {:value ""} "-- Select One --"]
-                 (map (fn [[area locations]]
-                        [:optgroup {:label area}
-                         (map (fn [[k v]] [:option {:value k} v]) locations)])
-                      locations)]
-                [:label       {:for  :boat} "Boat:"]
-                [:select#boat (merge {:name :boat}
-                                     (style {:min-width 0}))
-                 [:option {:value ""} "-- Select One --"]
-                 (map (fn [[k v]] [:option {:value k} v]) boats)]
-                [:p.total (style {:grid-column "1 / -1"
-                                  :font-size   "1.1em"
-                                  :margin      "1em 0 0.5em"}) "Subtotal: $" price]
-                [:button (merge {:type :submit}
-                                (style {:grid-column  "span 2"
-                                        :justify-self :center
-                                        :padding      "0.3em 1em"})) "Checkout"]]
-               (first (:body (almanac-request code)))]]]]
+             [:div.soft-outline (style {:display        :flex
+                                        :flex-direction :column
+                                        :flex           "1 1 50%"
+                                        :height         :min-content
+                                        :width          "calc(min(100%, 50ch))"
+                                        :margin         "5em auto"})
+              [:h1 (style {:font-size "1.5em"
+                           :margin    "0em auto 1em"}) "Purchase Popai"]
+              [:form.sku-selection (merge {:action (route/with-code route/checkout code)
+                                           :method :post}
+                                          (style {:display               :grid
+                                                  :grid-template-columns "auto 1fr"
+                                                  :gap                   "0.6em"
+                                                  :margin                "auto"}))
+               [:input {:type  :hidden
+                        :name  :product
+                        :value :popai}]
+               [:label           {:for  :location} "Location:"]
+               [:select#location (merge {:name :location}
+                                        (style {:min-width 0}))
+                [:option {:value ""} "-- Select One --"]
+                (map (fn [[area locations]]
+                       [:optgroup {:label area}
+                        (map (fn [[k v]] [:option {:value k} v]) locations)])
+                     locations)]
+               [:label       {:for  :boat} "Boat:"]
+               [:select#boat (merge {:name :boat}
+                                    (style {:min-width 0}))
+                [:option {:value ""} "-- Select One --"]
+                (map (fn [[k v]] [:option {:value k} v]) boats)]
+               [:p.total (style {:grid-column "1 / -1"
+                                 :font-size   "1.1em"
+                                 :margin      "1em 0 0.5em"}) "Subtotal: $" price]
+               [:button (merge {:type :submit}
+                               (style {:grid-column  "span 2"
+                                       :justify-self :center
+                                       :padding      "0.3em 1em"})) "Checkout"]]
+              (first (:body (almanac-request code)))]]]
      :script [(slurp (io/resource "almanac-checkout.js"))
               (slurp (io/resource "faq.js"))]}))
 
